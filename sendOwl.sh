ff22f650f7ca79af40e85f333be920bdffe2c770
dismad <81990132+dismad@users.noreply.github.com>
2023-08-06 19:41:27 -0700

    Update sendOwl.sh

--- sendOwl.sh
+++ sendOwl.sh
@@ -2,10 +2,10 @@
 
 from="${1}" # $1 represent first argument
 to="${2}" # $2 represent second argument
-amount="${3}" # $3 represent second argument
-memo="${4}" # $4 represent second argument
-fee="${5}" # $5 represent second argument
-privPol="${6}" # $6 represent second argument
+amount="${3}" # $3 represent third argument
+memo="${4}" # $4 represent forth argument
+fee="${5}" # $5 represent fifth argument
+privPol="${6}" # $6 represent sixth argument
 
 
 myOwl="zcash-cli z_sendmany \"$from\" \"[{\\\"address\\\": \\\"$to\\\",\\\"amount\\\": $amount, \\\"memo\\\":\\\"$memo\\\"}]\" 1 $fee $privPol"
